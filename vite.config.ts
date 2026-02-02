9e6181ef7e2c97ab5ef65a22fd4f80cf807c55ed
OleksandrTat <camilaveinte@gmail.com>
2026-02-02 09:45:08 +0100

    Fix CI build: remove tsc from build

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ export default defineConfig({
   base: '/viteWeb_AppWeb/',
   server: {
     host: true,
-    port: 5173,
-    allowedHosts: [
-      'amiee-vacillant-folly.ngrok-free.dev'
-    ]
+    port: 5173
   }
 })
